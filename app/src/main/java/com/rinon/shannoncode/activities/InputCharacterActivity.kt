13bdf07cd7dd96700aff9071fd453e8bd9baa280
rinon <rinonnon72@gmail.com>
2017-11-14 01:32:34 +0900

    Fixed id generation issue

--- app/src/main/java/com/rinon/shannoncode/activities/InputCharacterActivity.kt
+++ app/src/main/java/com/rinon/shannoncode/activities/InputCharacterActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TableRow
 import com.rinon.shannoncode.R
 import com.rinon.shannoncode.models.ShannonCode
 import kotlinx.android.synthetic.main.activity_input_character.*
 
 class InputCharacterActivity : AppCompatActivity() {
     companion object {
-        var pairList = ArrayList<Pair<EditText, EditText>>()        // first:char second:probability
+        enum class Location(val value: Int) {
+            Character(0),
+            Probability(1),
+
+            Max(2)
+        }
+
+        val pairList = ArrayList<Pair<EditText, EditText>>()        // first:char second:probability
         val RESULT = "result"
     }
 
@@ -29,56 +37,11 @@ class InputCharacterActivity : AppCompatActivity() {
         val num = intent.getIntExtra(InputNumberActivity.NUMBER, 0)
 
         for(counter in 0 until num) {
-            val row = layoutInflater.inflate(R.layout.container_input_character, scroll_view_content, true)
-            val char = row.findViewById<EditText>(R.id.edit_character)
-            val probability = row.findViewById<EditText>(R.id.edit_probability)
+            val row = layoutInflater.inflate(R.layout.container_input_character, scroll_view_content, false) as TableRow
+            val char = row.getChildAt(Location.Character.value) as EditText
+            val probability = row.getChildAt(Location.Probability.value) as EditText
+            scroll_view_content.addView(row)
 
-            // TODO: 処理の仕方がわからなすぎるのでとりあえずはこれ
-            when(counter) {
-                0 -> {
-                    char.id = R.id.character_input_character0
-                    probability.id = R.id.probability_input_character0
-                }
-                1 -> {
-                    char.id = R.id.character_input_character1
-                    probability.id = R.id.probability_input_character1
-                }
-                2 -> {
-                    char.id = R.id.character_input_character2
-                    probability.id = R.id.probability_input_character2
-                }
-                3 -> {
-                    char.id = R.id.character_input_character3
-                    probability.id = R.id.probability_input_character3
-                }
-                4 -> {
-                    char.id = R.id.character_input_character4
-                    probability.id = R.id.probability_input_character4
-                }
-                5 -> {
-                    char.id = R.id.character_input_character5
-                    probability.id = R.id.probability_input_character5
-                }
-                6 -> {
-                    char.id = R.id.character_input_character6
-                    probability.id = R.id.probability_input_character6
-                }
-                7 -> {
-                    char.id = R.id.character_input_character7
-                    probability.id = R.id.probability_input_character7
-                }
-                8 -> {
-                    char.id = R.id.character_input_character8
-                    probability.id = R.id.probability_input_character8
-                }
-                9 -> {
-                    char.id = R.id.character_input_character9
-                    probability.id = R.id.probability_input_character9
-                }
-                else -> {
-                    // ありえないのでエラー処理
-                }
-            }
             pairList.add(Pair<EditText, EditText>(char, probability))
         }
     }
